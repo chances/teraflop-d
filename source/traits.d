4e7d425f507d36ef5853d5f0185a7e8a2c8b4c4a
Chance Snow <git@chancesnow.me>
2020-10-28 17:02:28 -0500

    Add `isStruct`, `isClass`, and `inheritsFrom` helper traits

---
+++ source/traits.d
@@ -0,0 +1,14 @@
+module teraflop.traits;
+
+/// Detect whether `T` is a struct type.
+package enum bool isStruct(T) = is (T == struct);
+/// Detect whether `T` is a class type.
+package enum bool isClass(T) = is (T == class);
+
+/// Detect whether `T` inherits from `U`.
+template inheritsFrom(T, U) if (isClass!T && isClass!U) {
+  import std.meta : anySatisfy;
+  import std.traits : BaseClassesTuple;
+  enum bool isBaseClassOfU(T) = __traits(isSame, fullyQualifiedName!T, fullyQualifiedName!U);
+  enum bool inheritsFrom = anySatisfy!(isBaseClassOfU, BaseClassesTuple!T);
+}
