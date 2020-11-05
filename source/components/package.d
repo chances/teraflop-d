10977163d926f71ed27d5154ade7c90c61bbbfc9
Chance Snow <git@chancesnow.me>
2020-11-05 00:38:55 -0600

    Remove IDisposable, structs and classes can have destructors

--- source/components/package.d
+++ source/components/package.d
@@ -5,31 +5,13 @@
 /// License: 3-Clause BSD License
 module teraflop.components;
 
-import concepts : implements;
-import std.meta : templateAnd, templateOr;
-
-import teraflop.ecs : isComponent, storableAsComponent;
 import teraflop.time : Time;
-import teraflop.traits : isStruct;
 import teraflop.vulkan : Device;
 
 /// A Component that should be updated every frame.
 interface IUpdatable {
   /// Update the component given the current `Time`.
   void update(Time gameTime);
-}
-
-/// Detect whether `T` is a disposable Component.
-template isDisposable(T) {
-  alias implementsDisposable(T) = implements!(T, IDisposable);
-  alias isDisposableStruct = templateAnd!(isStruct, implementsDisposable);
-  enum bool isDisposable = storableAsComponent!T && (isDisposableStruct!T || isComponent!T);
-}
-
-/// Provides a mechanism for releasing unmanaged resources.
-interface IDisposable {
-  /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-  void dispose();
 }
 
 /// A Component that holds one or more handles to GPU resources.
