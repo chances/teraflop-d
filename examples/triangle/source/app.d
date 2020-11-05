a8283f9b7de933aed34b3a849819356dd3353f2e
Chance Snow <git@chancesnow.me>
2020-11-05 02:18:27 -0600

    Load shaders in triangle example

--- examples/triangle/source/app.d
+++ examples/triangle/source/app.d
@@ -10,11 +10,18 @@ void main()
 }
 
 private final class Triangle : Game {
+  import teraflop.graphics : Shader, ShaderStage;
+
   this() {
     super("Triangle");
   }
 
   override void initializeWorld() {
+    auto shaders = [
+      new Shader(ShaderStage.vertex, "examples/triangle/assets/shaders/triangle.vs.spv"),
+      new Shader(ShaderStage.fragment, "examples/triangle/assets/shaders/triangle.fs.spv")
+    ];
+
     // TODO: Add a triangle Entity
   }
 }
