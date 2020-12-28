4d5195289b11d03a73a14593c533bccacdb82812
Chance Snow <git@chancesnow.me>
2020-12-28 01:36:54 -0600

    Export `register` symbol from assembly

--- source/scripting/api/index.js
+++ source/scripting/api/index.js
@@ -1,5 +1,13 @@
 const fs = require("fs");
 const loader = require("@assemblyscript/loader");
-const imports = { /* imports go here */ };
+
+const registry = {}
+
+const imports = {
+  plugin: {
+    register: (name, version) => registry[name] = version
+  }
+};
 const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
 module.exports = wasmModule.exports;
+module.registry = registry;
