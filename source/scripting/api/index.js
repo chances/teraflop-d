6d2e03bbd13408de588965f6af66affc0cc78b40
Chance Snow <git@chancesnow.me>
2020-12-29 00:44:11 -0600

    Fix plugin registration in dependant JS clients

--- source/scripting/api/index.js
+++ source/scripting/api/index.js
@@ -1,14 +1,15 @@
-const fs = require("fs")
-const loader = require("@assemblyscript/loader")
+const fs = require('fs')
+const path = require('path')
+const loader = require('@assemblyscript/loader')
 
 const lib = require('./lib')
 
 const registry = {}
 
-const imports = {
-  plugin: {
-    register: (namePtr, version) => {
-      const name = __getString(namePtr)
+function registrarFor(moduleGetter) {
+  return (namePtr, version) => {
+    try {
+      const name = moduleGetter().exports.__getString(namePtr)
 
       registry[name] = {
         name,
@@ -16,10 +17,20 @@ const imports = {
       }
       console.log(`Registering plugin '${name}' v${registry[name].version.major}.${registry[name].version.minor}.${registry[name].version.patch}-${registry[name].version.meta}`)
       return version > 0
+    } catch {
+      return false
     }
   }
 }
-const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports)
-const { __getString } = wasmModule.exports
+
+const imports = {
+  plugin: {
+    register: registrarFor(() => wasmModule)
+  }
+}
+const wasmModule = loader.instantiateSync(fs.readFileSync(path.join(__dirname, 'build/optimized.wasm')), imports)
 module.exports = wasmModule.exports
 module.exports.plugin.unpackVersion = lib.unpackVersion
+module.exports.registry = {
+  registrarFor
+}
