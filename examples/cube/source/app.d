ac2c461f9c11411294279c7b7bfbc7f8162076a3
Chance Snow <git@chancesnow.me>
2021-01-04 03:51:12 -0600

    Render a full cube in the cube example

--- examples/cube/source/app.d
+++ examples/cube/source/app.d
@@ -2,6 +2,7 @@ import std.stdio;
 
 import teraflop.game : Game;
 import teraflop.graphics;
+static import teraflop.graphics.primitives;
 import teraflop.math;
 import teraflop.platform : Window;
 import teraflop.time : Time;
@@ -21,11 +22,15 @@ private final class Cube : Game {
   }
 
   override void initializeWorld(scope World world) {
+    import std.algorithm : map;
+    import std.conv : to;
+    import std.array : array;
+
     const framebufferSize = world.resources.get!Window.framebufferSize;
     auto camera = new Camera();
-    camera.view = mat4f.lookAt(vec3f(2f), vec3f(0), vec3f(0, 0, 1));
+    camera.view = mat4f.lookAt(vec3f(8f), vec3f(0), up);
     camera.projection = mat4f.perspective(
-      45.radians, framebufferSize.width / cast(float) framebufferSize.height, 0.05f, 10.0f
+      45.radians, framebufferSize.width / cast(float) framebufferSize.height, 0.01f, 1000.0f
     );
     world.resources.add(camera);
 
@@ -34,21 +39,15 @@ private final class Cube : Game {
       new Shader(ShaderStage.fragment, "examples/cube/assets/shaders/cube.fs.spv")
     ];
 
-    world.spawn(new Material(shaders, FrontFace.counterClockwise, CullMode.none), new Mesh!VertexPosColor([
-      VertexPosColor(vec3f(-0.5f, -0.5f, 0.0f), Color.red.vec3f),
-      VertexPosColor(vec3f(0.5f, -0.5f, 0.0f), Color.green.vec3f),
-      VertexPosColor(vec3f(0.5f, 0.5f, 0.0f), Color.blue.vec3f),
-      VertexPosColor(vec3f(-0.5f, 0.5f, 0.0f), Color(1, 0, 1).vec3f),
+    auto colors = [Color.red.vec3f, Color.green.vec3f, Color.blue.vec3f, Color(1, 0, 1).vec3f];
+    auto color = colors[3];
+    auto cube = teraflop.graphics.primitives.cube();
+    auto mesh = cube.vertices.map!(v => VertexPosColor(v.position, color)).array;
 
-      VertexPosColor(vec3f(-0.5f, -0.5f, 0.5f), Color.red.vec3f),
-      VertexPosColor(vec3f(0.5f, -0.5f, 0.5f), Color.green.vec3f),
-      VertexPosColor(vec3f(0.5f, 0.5f, 0.5f), Color.blue.vec3f),
-      VertexPosColor(vec3f(-0.5f, 0.5f, 0.5f), Color(1, 0, 1).vec3f),
-    ], [
-      0, 1, 2, 2, 3, 0,
-      4, 5, 6, 6, 7, 4
-    ]));
-
+    world.spawn(
+      new Material(shaders, FrontFace.clockwise),
+      new Mesh!VertexPosColor(Primitive.triangleList, mesh, cube.indices.to!(uint[]))
+    );
     this.add(System.from!aspectRatio);
     this.add(System.from!rotate);
   }
@@ -56,7 +55,7 @@ private final class Cube : Game {
   static void aspectRatio(scope const Window window, scope Camera camera) {
     const framebufferSize = window.framebufferSize;
     camera.projection = mat4f.perspective(
-      45.radians, framebufferSize.width / cast(float) framebufferSize.height, 0.05f, 10.0f
+      45.radians, framebufferSize.width / cast(float) framebufferSize.height, 0.01f, 1000.0f
     );
   }
 
